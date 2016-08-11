661070bc52450839ff2209b743a681d12de54a9f
Leon Wu <levey.leonhardt1@gmail.com>
2016-08-12 00:08:28 +1000

    -Added: SlideableItem, which provides setUsername() and PullToLimit interface. DensityUtil, which can change dp to px or px to dp.
    -Usage: Please implement the interface before using SlideableItem, more functions to be developed.

---
+++ app/src/main/java/com/unimelb/feelinglucky/snapsheet/Util/DensityUtil.java
@@ -0,0 +1,19 @@
+package com.unimelb.feelinglucky.snapsheet.Util;
+
+import android.content.Context;
+
+/**
+ * Created by leveyleonhardt on 1/26/16.
+ */
+public class DensityUtil {
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
