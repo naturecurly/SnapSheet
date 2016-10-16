6f921752e6c1bdb2735d2ad0fe368d62a78d085e
DavidFancy <yuhaoliu@192-168-1-101.tpgi.com.au>
2016-10-16 22:51:17 +1100

    greendao added

--- app/src/main/java/com/unimelb/feelinglucky/snapsheet/Story/Story.java
+++ app/src/main/java/com/unimelb/feelinglucky/snapsheet/Story/Story.java
@@ -1,16 +1,54 @@
 package com.unimelb.feelinglucky.snapsheet.Story;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.greenrobot.greendao.annotation.Convert;
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.converter.PropertyConverter;
+
 import java.io.Serializable;
 import java.util.List;
+import org.greenrobot.greendao.annotation.Generated;
 
 /**
  * Created by yuhaoliu on 1/10/16.
  */
+@Entity
 public class Story implements StoryInterface, Serializable {
     private String profileUrl;
     private String text;
+    @Convert(columnType = String.class, converter = Story.StringConverter.class)
     private List<String> imgUrls;
     private String timePassedText;
+
+    @Generated(hash = 1100848824)
+    public Story(String profileUrl, String text, List<String> imgUrls, String timePassedText) {
+        this.profileUrl = profileUrl;
+        this.text = text;
+        this.imgUrls = imgUrls;
+        this.timePassedText = timePassedText;
+    }
+
+    @Generated(hash = 922655990)
+    public Story() {
+    }
+
+    public static class StringConverter implements PropertyConverter<List<String>, String> {
+        @Override
+        public List<String> convertToEntityProperty(String databaseValue) {
+            Gson gson = new Gson();
+            List<String> rslt = gson.fromJson(databaseValue, new TypeToken<List<String>>(){}.getType());
+            return rslt;
+        }
+
+        @Override
+        public String convertToDatabaseValue(List<String> entityProperty) {
+            Gson gson = new Gson();
+            String rslt = gson.toJson(entityProperty);
+            return rslt;
+        }
+    }
 
     public void setTimePassedText(String timePassedText) {
         this.timePassedText = timePassedText;
@@ -46,5 +84,17 @@ public class Story implements StoryInterface, Serializable {
     @Override
     public String getTimePassedString() {
         return timePassedText;
+    }
+
+    public String getTimePassedText() {
+        return this.timePassedText;
+    }
+
+    public List<String> getImgUrls() {
+        return this.imgUrls;
+    }
+
+    public String getText() {
+        return this.text;
     }
 }
